9e7dfbffed7b367fb0e7ac91ce80da10a09dad23
Alan Henrique Nunes De Souza <alan.souza@b2wdigital.com>
2020-10-20 23:54:04 -0300

    remove console.log

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,8 +2,6 @@ import ReactGA from 'react-ga';
 
 export const onServiceWorkerUpdateReady = () => window.location.reload();
 
-console.log(process.env.GATSBY_GOOGLE_ANALYTICS_ID || 'none');
-
 ReactGA.initialize(process.env.GATSBY_GOOGLE_ANALYTICS_ID || 'none');
 
 export const onRouteUpdate = (state) => {
