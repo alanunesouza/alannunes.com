18dbebb1f0bbc9e2526da4c10bfc59b433e41380
Alan Henrique Nunes De Souza <alan.souza@b2wdigital.com>
2020-10-20 23:29:11 -0300

    teste console trackingId

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,9 @@ import ReactGA from 'react-ga';
 
 export const onServiceWorkerUpdateReady = () => window.location.reload();
 
-ReactGA.initialize(process.env.GOOGLE_ANALYTICS_ID);
+console.log(process.env.GOOGLE_ANALYTICS_ID || 'none');
+
+ReactGA.initialize(process.env.GOOGLE_ANALYTICS_ID || 'none');
 
 export const onRouteUpdate = (state) => {
   ReactGA.pageview(state.location.pathname);
--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId: process.env.GOOGLE_ANALYTICS_ID || 'none',
         head: false,
       },
     },
